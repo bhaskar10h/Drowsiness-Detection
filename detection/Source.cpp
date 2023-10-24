f4e6e87c7e83fddf6a42f4492996aeddc5ce4b2a
bhaskar10h <112790780+bhaskar10h@users.noreply.github.com>
2023-10-24 20:11:41 +0530

    Update Source.cpp
    Added a few more features to calibrate the detection to even more accruate

--- detection/Source.cpp
+++ detection/Source.cpp
@@ -22,11 +22,9 @@ cv::Point p;
 
 double compute_EAR(std::vector<cv::Point> vec)
 {
-
     double a = cv::norm(cv::Mat(vec[1]), cv::Mat(vec[5]));
     double b = cv::norm(cv::Mat(vec[2]), cv::Mat(vec[4]));
     double c = cv::norm(cv::Mat(vec[0]), cv::Mat(vec[3]));
-    //compute EAR
     double ear = (a + b) / (2.0 * c);
     return ear;
 }
@@ -40,17 +38,21 @@ int main()
             cerr << "Unable to connect to camera" << endl;
             return 1;
         }
-        cap.set(CV_CAP_PROP_FRAME_WIDTH, 640); //use small resolution for fast processing
+        cap.set(CV_CAP_PROP_FRAME_WIDTH, 640);
         cap.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
 
-        // Load face detection and deserialize  face landmarks model.
         frontal_face_detector detector = get_frontal_face_detector();
-
         deserialize("/path/to/model/shape_predictor_68_face_landmarks.dat") >> sp;
+        
+        // Create a pose predictor for head pose estimation
+        anypoint_model model = pose_predictor<anypoint_model>(sp);
 
-        // Grab and process frames until the main window is closed by the user.
+        bool isBlinking = false;
+        int blinkCount = 0;
+
+        cv::VideoWriter writer("output.avi", cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), 30, cv::Size(640, 480));
+
         while (!win.is_closed()) {
-            // Grab a frame
             cv::Mat temp;
             if (!cap.read(temp)) {
                 break;
@@ -59,17 +61,17 @@ int main()
             cv_image<bgr_pixel> cimg(temp);
             full_object_detection shape;
 
-            // Detect faces
             std::vector<rectangle> faces = detector(cimg);
             cout << "Number of faces detected: " << faces.size() << endl;
 
             win.clear_overlay();
             win.set_image(cimg);
 
-            // Find the pose of each face.
-            if (faces.size() > 0) {
+            for (size_t i = 0; i < faces.size(); ++i) {
+                shape = sp(cimg, faces[i]);
 
-                shape = sp(cimg, faces[0]); //work only with 1 face
+                // Head Pose Estimation
+                full_object_detection pose = model(cimg, faces[i]);
 
                 for (int b = 36; b < 42; ++b) {
                     p.x = shape.part(b).x();
@@ -81,35 +83,50 @@ int main()
                     p.y = shape.part(b).y();
                     righteye.push_back(p);
                 }
-                //Compute Eye aspect ration for eyes
+
                 double right_ear = compute_EAR(righteye);
                 double left_ear = compute_EAR(lefteye);
 
-                //if the avarage eye aspect ratio of lef and right eye less than 0.2, the status is sleeping.
+                if (left_ear < 0.2 && right_ear < 0.2) {
+                    if (!isBlinking) {
+                        blinkCount++;
+                        isBlinking = true;
+                    }
+                } else {
+                    isBlinking = false;
+                }
+
+                // Drawing overlay based on EAR
                 if ((right_ear + left_ear) / 2 < 0.2)
-                    win.add_overlay(dlib::image_window::overlay_rect(faces[0], rgb_pixel(255, 255, 255), "Sleeping"));
+                    win.add_overlay(dlib::image_window::overlay_rect(faces[i], rgb_pixel(255, 255, 255), "Sleeping"));
                 else
-                    win.add_overlay(dlib::image_window::overlay_rect(faces[0], rgb_pixel(255, 255, 255), "Not sleeping"));
+                    win.add_overlay(dlib::image_window::overlay_rect(faces[i], rgb_pixel(255, 255, 255), "Not sleeping"));
 
                 righteye.clear();
                 lefteye.clear();
 
                 win.add_overlay(render_face_detections(shape));
-
-                c = (char)waitKey(30);
-                if (c == 27)
-                    break;
             }
+
+            // Record the frame
+            writer.write(temp);
+
+            c = (char)waitKey(30);
+            if (c == 27)
+                break;
         }
-    }
-    catch (serialization_error& e) {
+
+        writer.release();  // Release the video writer
+
+    } catch (serialization_error& e) {
         cout << "Check the path to dlib's default face landmarking model file to run this example." << endl;
         cout << "You can get it from the following URL: " << endl;
         cout << "   http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2" << endl;
         cout << endl
             << e.what() << endl;
-    }
-    catch (exception& e) {
+    } catch (exception& e) {
         cout << e.what() << endl;
     }
+
+    return 0;
 }
